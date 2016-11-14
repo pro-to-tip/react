920832c9aac2f73118f9d78fa67a65b068a0470b
pro-to-tip <nenjoi@yahoo.com>
2016-11-14 05:18:17 +0300

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -70,7 +70,7 @@ var TopBarBox = React.createClass({
         <nav className="top-bar" data-topbar role="navigation">
             <ul className="title-area">
                 <li className="name">
-                    <h1><a href="#">Chat Server</a></h1>
+                    <h1><a href="#">*</a></h1>
                 </li>
                 <li className="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
             </ul>
